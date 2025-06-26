c4e0068ff33919d4f3330e97e171f52dfd79f168
CastroRmz <chriscastro845@gmail.com>
2025-06-25 21:48:40 -0500

    Update SistemaTickets.php

--- Dashboard/Dashboard/SistemaTickets/SistemaTickets.php
+++ Dashboard/Dashboard/SistemaTickets/SistemaTickets.php
@@ -150,7 +150,7 @@ $tickets = $conn->query("
 <html lang="es">
 <head>
   <meta charset="UTF-8" />
-  <title>Sistema de Tickets - Casino</title>
+  <title>Sistema de Tickets - Rio Casino</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
   <style>
@@ -803,3 +803,4 @@ document.addEventListener('DOMContentLoaded', function() {
 </script>
 </body>
 </html>
+
